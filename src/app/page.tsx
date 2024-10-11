f722928b457a81505be740df3d8f8403af1b96fd
Jediel Ribeiro <39655206+ideeej@users.noreply.github.com>
2024-10-11 20:31:26 +0000

    Basic layout for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section className="">
-      <header>
-        <Link href="./">
-        <Image alt="" width="64" height="64" src="/public/wws-logo.webp" />
+      <header className="
+      flex items-center justify-center gap-4
+      p-2
+      bg-slate-300
+      ">
+        <Link href="./" className="hover:scale-105 active:scale-100 transition-all">
+          <Image alt="" width="64" height="64" src="/wws-logo.png" className="inline" />
         </Link>
+        <Link href="./" className="font-normal hover:text-slate-400 hover:scale-105 active:scale-100 transition-all">
+          Home
+        </Link>
+        <Link href="./quem-somos" className="font-normal hover:text-slate-400 hover:scale-105 active:scale-100 transition-all">
+          Quem somos?
+        </Link>
+        <Link href="./blog" className="font-normal hover:text-slate-400 hover:scale-105 active:scale-100 transition-all">
+          Blog
+        </Link>
+        <input type="text" placeholder="Pesquise..." className="caret-black placeholder-gray-600 border border-gray-600 rounded-lg p-1 bg-slate-300 text-slate-600 outline-none"/>
       </header>
       <main></main>
       <section></section>
